86eaf3fcbd9e7b5292b05e5df725ab3a4aa9805b
ICGU Developer <developer@icgu.prototype>
2026-06-10 21:24:40 -0700

    Add ICGU landing page

--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-// Baseline landing confirmation for your approval presentation
 Route::get('/', function () {
-    return [
-        'Institute' => 'Institute of Corporate Governance of Uganda (ICGU)',
-        'Ecosystem_Status' => 'Sandbox Active',
-        'Compliance_Baseline' => 'Uganda Companies Act 2012',
-        'Database_Cluster' => 'Supabase Managed PostgreSQL',
-    ];
+    return view('welcome');
 });
