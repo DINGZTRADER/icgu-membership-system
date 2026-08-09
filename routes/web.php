d31d951874f615f0099386a6618c466bcf1a26ea
Peter Wacha <128045830+DINGZTRADER@users.noreply.github.com>
2026-08-09 10:13:58 +0300

    Add permission-gated staff membership review routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,23 @@
 <?php
 
+declare(strict_types=1);
+
+use App\Http\Controllers\StaffMembershipApplicationController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::prefix('staff/membership')
+    ->middleware(['auth', 'permission:applications.view'])
+    ->group(function (): void {
+        Route::get('/applications', [StaffMembershipApplicationController::class, 'index']);
+        Route::get('/applications/{reference}', [StaffMembershipApplicationController::class, 'show']);
+
+        Route::middleware('permission:applications.review')->group(function (): void {
+            Route::post('/applications/{reference}/review', [StaffMembershipApplicationController::class, 'startReview']);
+            Route::post('/applications/{reference}/approve', [StaffMembershipApplicationController::class, 'approve']);
+            Route::post('/applications/{reference}/reject', [StaffMembershipApplicationController::class, 'reject']);
+        });
+    });
