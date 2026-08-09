97313e34ec38440d026d8930d13fd4ea6ef101eb
Peter Wacha <128045830+DINGZTRADER@users.noreply.github.com>
2026-08-09 10:16:27 +0300

    Complete staff membership routes

--- routes/web.php
+++ routes/web.php
@@ -2,16 +2,32 @@
 
 declare(strict_types=1);
 
+use App\Http\Controllers\StaffMemberController;
 use App\Http\Controllers\StaffMembershipApplicationController;
+use App\Http\Controllers\StaffMembershipDocumentController;
+use App\Http\Controllers\StaffOrganisationController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::prefix('staff/membership')
-    ->middleware(['auth', 'permission:applications.view'])
-    ->group(function (): void {
+Route::prefix('staff/membership')->middleware('auth')->group(function (): void {
+    Route::middleware('permission:members.view')->group(function (): void {
+        Route::get('/members', [StaffMemberController::class, 'index']);
+        Route::get('/members/{member}', [StaffMemberController::class, 'show']);
+    });
+
+    Route::middleware('permission:organisations.view')->group(function (): void {
+        Route::get('/organisations', [StaffOrganisationController::class, 'index']);
+        Route::get('/organisations/{organisation}', [StaffOrganisationController::class, 'show']);
+    });
+
+    Route::middleware('permission:documents.view')->group(function (): void {
+        Route::get('/documents/{document}/download', [StaffMembershipDocumentController::class, 'download']);
+    });
+
+    Route::middleware('permission:applications.view')->group(function (): void {
         Route::get('/applications', [StaffMembershipApplicationController::class, 'index']);
         Route::get('/applications/{reference}', [StaffMembershipApplicationController::class, 'show']);
 
@@ -21,3 +37,4 @@ Route::prefix('staff/membership')
             Route::post('/applications/{reference}/reject', [StaffMembershipApplicationController::class, 'reject']);
         });
     });
+});
