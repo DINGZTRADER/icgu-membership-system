03953997e89f0f702590cf1365a96161a6bf927e
Peter Wacha <128045830+DINGZTRADER@users.noreply.github.com>
2026-08-09 11:19:40 +0300

    Expose public membership credential verification

--- routes/api.php
+++ routes/api.php
@@ -4,11 +4,15 @@ declare(strict_types=1);
 
 use App\Http\Controllers\PublicMembershipApplicationController;
 use App\Http\Controllers\PublicMembershipBillingController;
+use App\Http\Controllers\PublicMembershipVerificationController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('membership')->group(function (): void {
     Route::get('/plans', [PublicMembershipApplicationController::class, 'plans'])
         ->middleware('throttle:60,1');
+
+    Route::get('/verify/{verificationCode}', [PublicMembershipVerificationController::class, 'show'])
+        ->middleware('throttle:120,1');
 
     Route::post('/applications', [PublicMembershipApplicationController::class, 'store'])
         ->middleware('throttle:10,1');
