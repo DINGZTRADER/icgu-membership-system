c335a1232e734fd547215e86f69a6caa005f0eb1
Peter Wacha <128045830+DINGZTRADER@users.noreply.github.com>
2026-08-09 10:19:21 +0300

    Throttle public membership application endpoints

--- routes/api.php
+++ routes/api.php
@@ -6,9 +6,18 @@ use App\Http\Controllers\PublicMembershipApplicationController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('membership')->group(function (): void {
-    Route::get('/plans', [PublicMembershipApplicationController::class, 'plans']);
-    Route::post('/applications', [PublicMembershipApplicationController::class, 'store']);
-    Route::get('/applications/{reference}', [PublicMembershipApplicationController::class, 'show']);
-    Route::post('/applications/{reference}/documents', [PublicMembershipApplicationController::class, 'uploadDocument']);
-    Route::post('/applications/{reference}/submit', [PublicMembershipApplicationController::class, 'submit']);
+    Route::get('/plans', [PublicMembershipApplicationController::class, 'plans'])
+        ->middleware('throttle:60,1');
+
+    Route::post('/applications', [PublicMembershipApplicationController::class, 'store'])
+        ->middleware('throttle:10,1');
+
+    Route::get('/applications/{reference}', [PublicMembershipApplicationController::class, 'show'])
+        ->middleware('throttle:60,1');
+
+    Route::post('/applications/{reference}/documents', [PublicMembershipApplicationController::class, 'uploadDocument'])
+        ->middleware('throttle:30,1');
+
+    Route::post('/applications/{reference}/submit', [PublicMembershipApplicationController::class, 'submit'])
+        ->middleware('throttle:10,1');
 });
