cecd4182beb4610f580db198ba4e6959172d6cbd
Peter Wacha <128045830+DINGZTRADER@users.noreply.github.com>
2026-08-10 06:54:58 +0300

    Make staff MFA configurable for pilot

--- config/production.php
+++ config/production.php
@@ -6,5 +6,6 @@ return [
     'allow_local_document_storage' => (bool) env('PRODUCTION_ALLOW_LOCAL_DOCUMENT_STORAGE', false),
     'require_live_mail' => (bool) env('PRODUCTION_REQUIRE_LIVE_MAIL', true),
     'require_mtn_momo' => (bool) env('PRODUCTION_REQUIRE_MTN_MOMO', false),
+    'require_staff_mfa' => (bool) env('STAFF_MFA_REQUIRED', false),
     'pilot_import_max_rows' => (int) env('PILOT_IMPORT_MAX_ROWS', 50),
 ];
